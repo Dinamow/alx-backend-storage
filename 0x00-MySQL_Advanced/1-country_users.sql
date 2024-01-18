07f9630dc701b29a708af35f53aca60180139b08
DINAMOW <meemoo102039@gmail.com>
2024-01-18 23:02:34 +0200

    fix

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,9 @@
 -- Creates a table with unique users.
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US',
+    PRIMARY KEY (id)
 );
